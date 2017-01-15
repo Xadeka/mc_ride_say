b1416729746e78e6b0fd412c42ed6ca899eec511
Zade Kaakarni <xade.kaakarni@gmail.com>
2017-01-15 00:04:12 -0600

    Setting up imgs to have consistent naming

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
+
+  var img_file_prefix = 'img/mc_ride';
+  var img_file_ext = '.png';
+
+  var mc_ride = $('#mc-ride');
+
   $('#btn-mc-ride-say').on('click', function() {
     var audio = new Audio('sounds/yell.wav');
     audio.play();
-    $('#mc-ride').toggleClass('rotate-left');
+    mc_ride.attr('src', img_file_prefix + '1' + img_file_ext);
+    mc_ride.toggleClass('rotate-left');
         setTimeout(function(){
-      $('#mc-ride').toggleClass('rotate-left');
-      $('#mc-ride').toggleClass('rotate-right');
+      mc_ride.toggleClass('rotate-left');
+      mc_ride.toggleClass('rotate-right');
       setTimeout(function(){
-        $('#mc-ride').toggleClass('rotate-right');
+        mc_ride.toggleClass('rotate-right');
+        setTimeout(function(){
+          mc_ride.attr('src', img_file_prefix + '0' + img_file_ext);
+        },1000);
       }, 1000);
     }, 1000);
   });
