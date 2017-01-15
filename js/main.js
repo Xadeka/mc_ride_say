4e4d07b4d8a52b40e471bfa1e00c735a303de1d3
Zade Kaakarni <xade.kaakarni@gmail.com>
2017-01-15 07:32:25 -0600

    Click MC Ride himself to get a fortune. Added more pics. Pics random

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 
   var img_file_prefix = 'img/mc_ride';
   var img_file_ext = '.png';
+  var numOfImgs = 4; //not including default
 
   var mc_ride = $('#mc-ride');
 
-  $('#btn-mc-ride-say').on('click', function() {
+  mc_ride.on('click', function() {
     var audio = new Audio('sounds/yell.wav');
     audio.play();
-    mc_ride.attr('src', img_file_prefix + '1' + img_file_ext);
+    var randImg = Math.floor(Math.random() * (numOfImgs)) + 1;
+    mc_ride.attr('src', img_file_prefix + randImg + img_file_ext);
     mc_ride.toggleClass('rotate-left');
         setTimeout(function(){
       mc_ride.toggleClass('rotate-left');
