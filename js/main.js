dd6a590e7624017509e7c70bbc13a3ce7a3e324b
Zade Kaakarni <xade.kaakarni@gmail.com>
2017-01-15 08:27:42 -0600

    Randomly picks audio file to use

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
   var img_file_ext = '.png';
   var numOfImgs = 4; //not including default
 
+  var wav_file_prefix = 'sounds/mc_ride';
+  var wav_file_ext = '.wav';
+  var numOfAudio = 4;
+
   var mc_ride = $('#mc-ride');
 
   mc_ride.on('click', function() {
-    var audio = new Audio('sounds/yell.wav');
+    var rand_wav = Math.floor(Math.random() * (numOfAudio - 0));
+    var audio = new Audio(wav_file_prefix + rand_wav + wav_file_ext);
     audio.play();
     var randImg = Math.floor(Math.random() * (numOfImgs)) + 1;
     mc_ride.attr('src', img_file_prefix + randImg + img_file_ext);
