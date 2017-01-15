b047be3eed41295da43a437fa78cbb100f740065
Zade Kaakarni <xade.kaakarni@gmail.com>
2017-01-14 23:46:40 -0600

    Added ability to play sounds on click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $('#btn-mc-ride-say').on('click', function() {
+    var audio = new Audio('sounds/yell.wav');
+    audio.play();
     $('#mc-ride').toggleClass('rotate-left');
-    setTimeout(function(){
+        setTimeout(function(){
       $('#mc-ride').toggleClass('rotate-left');
       $('#mc-ride').toggleClass('rotate-right');
       setTimeout(function(){
         $('#mc-ride').toggleClass('rotate-right');
       }, 1000);
-
     }, 1000);
   });
 });
